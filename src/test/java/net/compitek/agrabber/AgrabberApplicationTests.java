6a54e9cecd343bbfc5dc2871deb7c05449c3e549
Evgene Krylov <compitek.net@gmail.com>
2020-01-12 08:39:38 +0300

    initial

---
+++ src/test/java/net/compitek/agrabber/AgrabberApplicationTests.java
@@ -0,0 +1,13 @@
+package net.compitek.agrabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrabberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
